5ba3766c6d266b9f8e2c5038bd2737bdbacd7e47
npex42 <npex42@gmail.com>
2021-05-13 13:25:19 +0100

    Setup Allocation & Heap

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![no_std]
+#![feature(decl_macro)]
+#![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)] // at the top of the file
+
+extern crate alloc;
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
+
+pub mod paging;
+pub mod memory;
+pub mod heap;
