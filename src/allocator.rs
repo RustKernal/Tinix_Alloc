42650e2d59724bcef91eee73fb39d57262ea79e0
NPEX42 <NPEX42@gmail.com>
2021-05-16 19:10:27 +0100

    Changed Heap Size to 1MiB

--- src/allocator.rs
+++ src/allocator.rs
@@ -5,8 +5,8 @@ use x86_64::{
     VirtAddr,
 };
 
-pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 1024 * 1024 * 1; // 1MiB
+pub const HEAP_START: usize = 0x_4000_0000_0000;
+pub const HEAP_SIZE: usize =  1024 * 1024; // 1MiB
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
